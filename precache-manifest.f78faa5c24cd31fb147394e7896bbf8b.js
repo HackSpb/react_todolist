6430f44471304fdfeab166b285b55a2b947e52f9
hackspb <hackspb@gmail.com>
2020-06-15 06:21:54 +0300

    Updates

---
+++ precache-manifest.f78faa5c24cd31fb147394e7896bbf8b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c9896a82963b8c012ff25fc2a8b387fd",
+    "url": "/react_todolist/index.html"
+  },
+  {
+    "revision": "f3ceb2e3546e1408b3f7",
+    "url": "/react_todolist/static/css/main.0c8e3b29.chunk.css"
+  },
+  {
+    "revision": "21a5ba3eeca38f1c6fac",
+    "url": "/react_todolist/static/js/2.2e936b47.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/react_todolist/static/js/2.2e936b47.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f3ceb2e3546e1408b3f7",
+    "url": "/react_todolist/static/js/main.ba481030.chunk.js"
+  },
+  {
+    "revision": "617fb14696fa1c7e2d5c",
+    "url": "/react_todolist/static/js/runtime-main.b7e334d0.js"
+  }
+]);
